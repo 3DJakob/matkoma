388420076935f0ad73e4f4678623cc191f4015fb
Jakob <jakob@folkdatorn.se>
2019-11-15 10:11:25 +0100

    Blurred background component

---
+++ src/components/BlurredBackground.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function BlurredBackground ({backgroundURL}) {
+  const bgStyle = {
+    backgroundImage: 'url(' + backgroundURL + ')',
+    position: 'absolute',
+    top: -10,
+    bottom: -10,
+    right: -10,
+    left: -10,
+    backgroundSize: 'cover',
+    filter: 'blur(10px)',
+    backgroundPosition: 'center',
+    zIndex: -10,
+    transitionProperty: 'background',
+    transitionDuration: '0.25s'
+  }
+
+  return (
+    <div style={bgStyle}></div>
+  )
+}
+
+export default BlurredBackground
