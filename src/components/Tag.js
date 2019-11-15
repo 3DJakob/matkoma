c2e986c7ea200ea94c3df8c2fb4260879e1dccfe
Jenny Rudemo <jennyrudemo@gmail.com>
2019-11-15 11:51:03 +0100

    Create Tag and animations
    Component Tag that is animated according to animations.css

---
+++ src/components/Tag.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes} from '@fortawesome/free-solid-svg-icons'
+import '../css/animations.css'
+
+function Tag ({text, onClick}) {
+    const tagStyle = {
+        backgroundColor: '#ce4257',
+        color: '#ffffff',
+        fontFamily: '"Bree", sans-serif',
+        fontSize: 12,
+        fontWeight: 'regular',
+        border: 'none',
+        padding: 10,
+        margin: 5,
+        borderRadius: 100,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        animationName: 'createTag',
+        animationDuration: '0.5s'
+    }
+
+    const iconStyle = {
+        color: '#fff',
+        padding: 2,
+        marginLeft: 5
+    }
+
+    return (
+        <button type="button" style={tagStyle}>
+            {text}
+            <FontAwesomeIcon onClick={onClick} style={iconStyle} icon={faTimes}/>
+        </button>
+    )  
+}
+
+export default Tag
