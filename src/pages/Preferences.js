5d67b50809de52bc3eab4d260dbb4c537759a9cd
Jenny Rudemo <jennyrudemo@gmail.com>
2019-11-20 17:11:30 +0100

    Update Preferences.js
    Added components.

---
+++ src/pages/Preferences.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import LogoBanner from '../components/LogoBanner'
+import TitleWithDescription from '../components/TitleWithDescription'
+import SearchList from '../components/SearchList'
+import recepies from '../db/recepies'
+import getIngredientsFromRecepies from '../lib/utils'
+import Slider from '@material-ui/core/Slider'
+import Button from '../components/Button'
+import Checkbox from '../components/Checkbox'
+
+function Preferences () {
+  const ingredients = getIngredientsFromRecepies(recepies)
+  const onSelectedIngredient = (ingredient) => {
+    console.log(ingredient)
+  }
+  const [timerange, setTimerange] = React.useState([20, 80])
+
+  const changeTime = (event, newTime) => {
+    setTimerange(newTime)
+    console.log(timerange)
+  }
+
+  return (
+    <div className='Preferences'>
+      <LogoBanner />
+      <TitleWithDescription title='1. Lägg till ingredienser' description='Ange upp till tre ingredienser som du vill använda' />
+      <SearchList placeholder='Skriv in en ingrediens...' numberOfVisibleInResult={5} list={ingredients} onClick={onSelectedIngredient} />
+      <TitleWithDescription title='2. Ange (mål)tid' description='Hur mycket tid har du?' />
+      <Slider
+        value={timerange}
+        onChange={changeTime}
+        valueLabelDisplay='auto'
+      />
+      <TitleWithDescription title='3. Ange matpreferens' description='Har du någon specialkost?' />
+      <Checkbox label='Vegetariskt' />
+      <Checkbox label='Glutenfri' />
+      <Checkbox label='Laktosfri' />
+      <Checkbox label='Äggfri' />
+      <Checkbox label='Vegan' />
+      <Checkbox label='Nötfri' />
+      <Button text='Hitta match' upperCase onClick={() => ('Sökknappen klickt')} />
+
+    </div>
+  )
+}
+
+export default Preferences
