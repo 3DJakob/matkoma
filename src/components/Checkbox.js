f91d36f2ba088a7d7c07d529024b66303352b9c2
Jakob <jakob@folkdatorn.se>
2019-11-26 14:49:13 +0100

    Added animation, style prop, and onPress callback

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,63 +1,81 @@
 import React, { Component } from 'react'
+import '../css/animations.css'
 
 class Checkbox extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isChecked: false
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      isChecked: false
+    }
+  }
+
+  render () {
+    const { label } = this.props
+    const { isChecked } = this.state
+    const checkboxStyle = {
+      color: '#000',
+      fontFamily: '"Lato", sans-serif',
+      fontSize: '20px',
+      textAlign: 'right',
+      fontWeight: 'regular',
+      padding: '15px',
+      margin: '5px',
+      borderRadius: '100px',
+      border: '2px solid #6fc48c'
     }
 
-    toggleCheckboxChange = () => {
-        this.setState({
-            isChecked: !this.state.isChecked,
-        });
-
+    const checkboxRow = {
+      display: 'flex',
+      alignItems: 'center',
+      color: '#000'
     }
 
-    render() {
-        const { label } = this.props;
-        const { isChecked } = this.state;
-        console.log(isChecked);
-        let checkboxStyle = {
-            color: '#000',
-            fontFamily: '"Lato", sans-serif',
-            fontSize: '20px',
-            textAlign: 'right',
-            fontWeight: 'regular',
-            padding: '15px',
-            margin: '5px',
-            borderRadius: '100px',
-            border: '2px solid #6fc48c'
-        }
-
-        if(isChecked){
-            checkboxStyle.backgroundColor = '#6fc48c'
-        }else{
-            checkboxStyle.backgroundColor = '#fff'
-        }
-
-        const checkboxRow = {
-            display: 'flex',
-            alignItems: 'center',
-            padding: '20px',
-            margin: 'auto',
-            color: '#000'
-        }
-
-        return (
-            <div className='checkbox' style={checkboxRow}>
-                    <input
-                        type='checkbox'
-                        value={label}
-                        style={checkboxStyle}
-                        checked={this.state.isChecked}
-                        onChange={this.toggleCheckboxChange}
-                    />
-                    {label} 
-            </div>
-        )
+    const circleStyle = {
+      width: 20,
+      height: 20,
+      position: 'absolute',
+      marginLeft: 12,
+      borderRadius: '100%',
+      backgroundColor: '#6fc48c',
+      animationDuration: '0.3s',
+      animationName: isChecked ? 'createTag' : '',
+      animationFillMode: 'forwards',
+      transition: '200ms',
+      transform: isChecked ? 'scale(1)' : 'scale(0)',
+      pointerEvents: 'none'
     }
+
+    const labelStyle = {
+      display: 'flex',
+      marginLeft: 5,
+      outline: 'none',
+      height: '100%',
+      userSelect: 'none',
+      alignItems: 'center',
+      flexGrow: 1
+    }
+
+    const toggleCheckboxChange = () => {
+      this.setState({
+        isChecked: !this.state.isChecked
+      })
+      this.props.onPress(!this.state.isChecked)
+    }
+
+    return (
+      <div style={{ ...checkboxRow, ...this.props.style }}>
+        <input
+          type='checkbox'
+          id={label}
+          style={checkboxStyle}
+          checked={this.state.isChecked}
+          onChange={toggleCheckboxChange}
+        />
+        <div style={circleStyle} />
+        <label style={labelStyle} htmlFor={label}>{label}</label>
+      </div>
+    )
+  }
 }
 
 export default Checkbox
