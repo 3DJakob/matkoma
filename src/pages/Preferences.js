ce13c817bd95d42e87dd9d93672b3897aafaab08
Jakob <jakob@folkdatorn.se>
2019-11-27 13:52:30 +0100

    Updated preferences from map

--- src/pages/Preferences.js
+++ src/pages/Preferences.js
@@ -23,16 +23,18 @@ function Preferences ({ recepies }) {
   return (
     <div className='Preferences'>
       <LogoBanner />
-      <TitleWithDescription title='1. Lägg till ingredienser' description='Ange upp till tre ingredienser som du vill använda' />
-      <SearchList placeholder='Skriv in en ingrediens...' numberOfVisibleInResult={5} list={ingredients} onClick={onSelectedIngredient} />
-      <TitleWithDescription title='2. Ange (mål)tid' description='Hur mycket tid har du?' />
-      <StyledSlider onChange={() => console.log('Slidern slidedad')} />
-      <TitleWithDescription title='3. Ange matpreferens' description='Har du någon specialkost?' />
-      <div className='dietContainer'>
-        {specialDiets.map((diet) => <Checkbox style={{ width: '50%' }} key={diet} label={diet} onPress={(state) => toggleDiet(state, diet)} />)}
+      <div className='topBg' />
+      <div className='preferencesContainer'>
+        <TitleWithDescription title='1. Lägg till ingredienser' description='Ange upp till tre ingredienser som du vill använda' />
+        <SearchList placeholder='Skriv in en ingrediens...' numberOfVisibleInResult={5} list={ingredients} onClick={onSelectedIngredient} />
+        <TitleWithDescription title='2. Ange (mål)tid' description='Hur mycket tid har du?' />
+        <StyledSlider onChange={() => console.log('Slidern slidedad')} />
+        <TitleWithDescription title='3. Ange matpreferens' description='Har du någon specialkost?' />
+        <div className='dietContainer'>
+          {specialDiets.map((diet) => <Checkbox style={{ width: '50%' }} key={diet} label={diet} onPress={(state) => toggleDiet(state, diet)} />)}
+        </div>
+        <Button text='Hitta match' upperCase onClick={() => ('Sökknappen klickt')} />
       </div>
-      <Button text='Hitta match' upperCase onClick={() => ('Sökknappen klickt')} />
-
     </div>
   )
 }
