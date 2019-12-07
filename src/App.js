190423f774fcc5ea9a059d32ec116a8979fe98b9
Jakob <jakob@folkdatorn.se>
2019-12-07 13:35:18 +0100

    implemented preferences results

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import recepies from './db/recepies.json'
 
 function App () {
   const [route, SetRoute] = useState('preferences')
+  const [resultRecepies, setResultRecepies] = useState([])
+
+  const onFilteredRecepies = (recepies) => {
+    setResultRecepies(recepies)
+    SetRoute('swipe')
+  }
 
   return (
     <div className='App'>
       <meta name='viewport' content='width=device-width, user-scalable=no' />
       <link rel='stylesheet' href='https://use.typekit.net/kja2qvx.css' />
-      {route === 'preferences' ? <Preferences recepies={recepies} /> : ''}
-      {route === 'swipe' ? <Swipe recepies={recepies} /> : ''}
+      {route === 'preferences' ? <Preferences onRecepieResult={onFilteredRecepies} recepies={recepies} /> : ''}
+      {route === 'swipe' ? <Swipe recepies={resultRecepies} /> : ''}
     </div>
   )
 }
