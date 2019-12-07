0f84b32e0200c4c1695d3d47e617e037e44d44a3
andreas engberg <adde.engberg@gmail.com>
2019-12-07 17:36:23 +0100

    fixed recipes DB typo

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react'
 import './css/App.css'
 import Preferences from './pages/Preferences'
 import Swipe from './pages/Swipe'
-import recepies from './db/recepies.json'
+import recipes from './db/recipes.json'
 
 function App () {
-  const [route, SetRoute] = useState('preferences')
+  const [route, SetRoute] = useState('swipe')
 
   return (
     <div className='App'>
       <meta name='viewport' content='width=device-width, user-scalable=no' />
       <link rel='stylesheet' href='https://use.typekit.net/kja2qvx.css' />
-      {route === 'preferences' ? <Preferences recepies={recepies} /> : ''}
-      {route === 'swipe' ? <Swipe recepies={recepies} /> : ''}
+      {route === 'preferences' ? <Preferences recipes={recipes} /> : ''}
+      {route === 'swipe' ? <Swipe recipes={recipes} /> : ''}
     </div>
   )
 }
