fa861241ea5b5c075f019742024c58b1f757643b
Jakob <jakob@folkdatorn.se>
2019-12-07 17:05:11 +0100

    Scroll to top after navigation

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App () {
 
   return (
     <div className='App'>
+      {window.scrollTo(0, 0) /* scroll up on route change} */}
       <meta name='viewport' content='width=device-width, user-scalable=no' />
       <link rel='stylesheet' href='https://use.typekit.net/kja2qvx.css' />
       {route === 'preferences' ? <Preferences onRecepieResult={onFilteredRecepies} recepies={recepies} /> : ''}
