25d10d7fba07bd6973ef078fe3d6f570bc127c89
Jakob <jakob@folkdatorn.se>
2019-12-09 14:32:57 +0100

    Cards update onSwipe

--- src/pages/Swipe.js
+++ src/pages/Swipe.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import '../css/Swipe.css'
 import Card from '../components/Card'
 import Description from '../components/Description'
@@ -6,25 +6,48 @@ import BlurredBackground from '../components/BlurredBackground'
 import Banner from '../img/receptmatch.svg'
 import TinderCard from 'react-tinder-card'
 
-function Swipe ({ recepies }) {
-  const recepie = recepies[0]
+const cardsToShowAtTheTime = 4
 
-  const cardSwiped = (dir) => {
-    console.log('you swiped it!')
+function Swipe ({ recepies }) {
+  const buttonRef = useRef(null)
+  const [amountOfCardsToShow, setAmountOfCardsToShow] = useState(recepies.length - cardsToShowAtTheTime)
+  const [currentRecipe, setCurrentRecipe] = useState(recepies[recepies.length - 1])
+
+  useEffect(() => {
+    document.title = `You clicked ${amountOfCardsToShow} times`
+  })
+
+  const cardSwiped = (dir, index) => {
+    buttonRef.current.click()
+  }
+
+  const increase = () => {
+    setAmountOfCardsToShow(amountOfCardsToShow - 1)
+    setCurrentRecipe(recepies[amountOfCardsToShow + cardsToShowAtTheTime - 2])
+    if (amountOfCardsToShow - 1 === -cardsToShowAtTheTime) {
+      console.log('all recepies swiped!')
+    }
+  }
+
+  const hiddenStyle = {
+    display: 'none'
   }
 
   return (
     <div className='swipe'>
+      <button ref={buttonRef} style={hiddenStyle} onClick={increase}>increase!</button>
       <img className='banner' src={Banner} alt='' />
-      <BlurredBackground backgroundURL={recepie.imageURL} height='90vh' />
+      <BlurredBackground backgroundURL={currentRecipe.imageURL} height='90vh' />
       <div className='swipeArea'>
         <div className='CardContainer'>
-          <TinderCard className='tinderCard' onSwipe={cardSwiped} onCardLeftScreen={cardSwiped}>
-            <Card recipe={recepie} />
-          </TinderCard>
+          {recepies.map((recepie, index) =>
+            <TinderCard key={recepie.id} className={index > amountOfCardsToShow ? 'tinderCard' : 'tinderCard hidden'} onSwipe={cardSwiped} onCardLeftScreen={() => console.log('left sceen')}>
+              <Card key={recepie.id + 'card'} recipe={recepie} />
+            </TinderCard>
+          )}
         </div>
       </div>
-      <Description style={{ zIndex: 15 }} recipe={recepie} />
+      <Description style={{ zIndex: 15 }} recipe={currentRecipe} />
     </div>
 
   )
