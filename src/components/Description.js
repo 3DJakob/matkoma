5b6d47e3b833fee5830aa5b3636d4ab7b56b7f21
Jakob <jakob@folkdatorn.se>
2019-12-14 00:10:19 +0100

    Refined design to show numbers

--- src/components/Description.js
+++ src/components/Description.js
@@ -11,7 +11,7 @@ function Description ({ style = {}, recipe, onClick, arrowDown = false }) {
 
   const DescriptionWrapper = {
     position: 'relative',
-    margin: '-80px 0 0 0',
+    margin: '-105px 0 0 0',
     width: '100vw'
   }
 
