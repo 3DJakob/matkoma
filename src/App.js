0f53f4c5eda1698d60da8af8bcf45acc7ab610e0
Jakob <jakob@folkdatorn.se>
2019-12-18 11:08:35 +0100

    Added icons

--- src/App.js
+++ src/App.js
@@ -17,6 +17,13 @@ function App () {
   }
   return (
     <div className='App'>
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#6fc48c' />
+      <meta name='msapplication-TileColor' content='#6fc48c' />
+      <meta name='theme-color' content='#6fc48c' />
       <link href='https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap' rel='stylesheet' />
       {window.scrollTo(0, 0) /* scroll up on route change} */}
       <meta name='viewport' content='width=device-width, user-scalable=no' />
